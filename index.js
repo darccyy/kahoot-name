1c0acca9d5333df95604ef8d7ad375d866002bb5
DarccMan <darcy3057@gmail.com>
2022-03-11 12:49:35 +1100

    Url

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.listen(process.env.PORT || 3000, () => {
   console.log(
     "Listening on " +
       (process.env.PORT
-        ? "https://kahoot-name.herokuapp.com"
+        ? "https://kahootname.herokuapp.com"
         : "http://localhost:3000"),
   );
 });
